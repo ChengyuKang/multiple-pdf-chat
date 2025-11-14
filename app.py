eb90d2bf9f78eceae761cc6fbd035fbb3c90fda5
chengyukang <s1998909@ed.ac.uk>
2025-11-14 15:46:42 +0000

    use pdf reader to get pdf text

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 def main():
     load_dotenv()
@@ -9,8 +18,15 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documnets")
-        st.file_uploader("Upload your PDFs here and click on process")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on process", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+                # get text chunks
+
+                # create vector store
 
 if __name__ == '__main__':
     main()
